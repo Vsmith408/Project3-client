ae835e56086594bfd9b577cd886914fb723e97b1
Victoria Smith <victoriasmithh428@gmail.com>
2020-12-21 21:16:44 -0500

    created fetch requests for data and render to screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,61 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import './App.css';
+import React from "react";
+import SearchBar from "./SearchBar";
+import places from "../apis/places";
+import "../components/App.css";
 
+class App extends React.Component {
+  state = { hotels: [], food: [], interest: [], isLoading: false };
 
-class App extends React.Component{
-  render(){
-    return(
-      <SearchBar/>
-    )
+  filterData = (arr) => arr.map((item) => item.name);
+
+  onSearchSubmit = async (type) => {
+    this.setState({ isLoading: true });
+    try {
+      const response = await places.get(
+        `http://localhost:3001/api/google/places`,
+        {
+          params: { type },
+          headers: {
+            Authorization: "AIzaSyAgyAAPxiaxnD23FRxnI03LYR7TKavLk1Q",
+          },
+        }
+      );
+      this.setState({
+        hotels: this.filterData(response.data.hotels.results),
+        food: this.filterData(response.data.food.results),
+        interest: this.filterData(response.data.interest.results),
+      });
+    } catch (e) {
+      alert("Something broke");
+    } finally {
+      this.setState({ isLoading: false });
     }
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.isLoading ? (
+          <p>Loading....</p>
+        ) : (
+          <>
+            <h1>Hotels</h1>
+            <pre style={{ background: "white" }}>
+              {JSON.stringify(this.state.hotels, null, 2)}
+            </pre>
+            <h1>Food</h1>
+            <pre style={{ background: "white" }}>
+              {JSON.stringify(this.state.food, null, 2)}
+            </pre>
+            <h1>Points of Interest</h1>
+            <pre style={{ background: "white" }}>
+              {JSON.stringify(this.state.interest, null, 2)}
+            </pre>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 
-
-function Form(props){
-return (
-  <div className="container">
-  <form>
-    <div className="form-group">
-      <input
-        onChange={props.handleInputChange}
-        value={props.search}
-        name="search"
-        type="text"
-        className="form-control"
-        placeholder="Find your next adventure"
-        id="search"
-      />
-      <button onClick={props.handleFormSubmit} className="btn btn-light mt-3 mb-5">
-        Find Adventure
-        </button>
+function Form(props) {
+  return (
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <input
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Find your next adventure"
+            id="search"
+          />
+          <button
+            onClick={props.handleFormSubmit}
+            className="btn btn-light mt-3 mb-5"
+          >
+            Find Adventure
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
-  </div>
-);
+  );
 }
 
 export default Form;
