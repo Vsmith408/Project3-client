b9a585cdfd24b84e49ebf34d0995c9d66facd39e
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2021-01-11 17:40:55 -0500

    added some styling of favorites page

--- src/components/pages/Favs.js
+++ src/components/pages/Favs.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import API from '../../apis/API'
-import FavCard from '../FavCard'
+import React, { useEffect, useState } from 'react';
+import API from '../../apis/API';
+import FavCard from '../FavCard';
+import background from "../../Images/plan.jpg";
 
 export default function Favs() {
   //setting initial state
@@ -32,18 +33,36 @@ export default function Favs() {
   //         .then(res => loadFavs())
   //         .catch(err => console.log(err));
   // }
-
+    
+  
   return (
-    <div>
-      <h1>Bucket List</h1>
-      {Fav.length > 0
+    <div className="ui container" style={{ backgroundImage: `url(${background})`}}>
+      <h1 className="header">Bucket List</h1>
+      <div className="ui three column doubling stackable grid">
+          <div className="column">
+          {Fav.length > 0
         ? Fav.map((favs) => <FavCard {...favs} onDelete={loadFavs} />)
         : null}
+          </div>
+      </div>
     </div>
     //need to map out length to display x amount of results
     //could take from 21 activities and produce a list
   )
+  
 }
 
 //fix favcard
 //add remove from favs button -- (add this toggle feature to add button as well?)
+
+
+/*
+
+{Fav.length > 0 ? Fav.map((favs) =>(
+    <div className="column">
+      <FavCard {...favs} onDelete={loadFavs} />
+      : null
+    </div> 
+))}
+
+*/
